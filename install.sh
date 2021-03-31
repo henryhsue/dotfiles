74de4e4005edf37605674e20b5e31e2985e4ede3
Henry Hsue <33713116+henryhsue@users.noreply.github.com>
2021-03-31 11:06:30 -0700

    neovim init.vim points to vimrc

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -f -s ~/Personal/dotfiles/.tmux.conf ~/.tmux.conf
 ln -f -s ~/Personal/dotfiles/.gitconfig ~/.gitconfig
 ln -f -s ~/Personal/dotfiles/.zshrc ~/.zshrc # pulls bash .profile
 
+
 # installs homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -17,9 +18,15 @@ chsh -s $(which zsh)
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# neovim plug
+# install plug for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# point neovim's init.vim to ~/.vimrc
+mkdir -p ~/.config/nvim/
+ln -f -s ~/Personal/dotfiles/init.vim ~/.config/nvim/init.vim # init.vim points to ~/.vimrc
+
+# install plugins
 nvim -c PlugInstall
 
 # re-source envs
