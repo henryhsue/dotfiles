ed089b84f7b67703915de8a2d772a070d3ffd57b
Henry Hsue <33713116+henryhsue@users.noreply.github.com>
2021-03-31 09:55:00 -0700

    rm .work; update install

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ ln -f -s ~/Personal/dotfiles/.vimrc ~/.vimrc
 ln -f -s ~/Personal/dotfiles/.tmux.conf ~/.tmux.conf
 ln -f -s ~/Personal/dotfiles/.gitconfig ~/.gitconfig
 ln -f -s ~/Personal/dotfiles/.zshrc ~/.zshrc
-ln -f -s ~/Personal/dotfiles/.work ~/.work
 
-# install everything in Brewfile using Homebrew and Cask
-# brew bundle
+# installs homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install everything in Brewfile using Homebrew and Cask. Installs neovim.
+brew bundle
 
 # zsh
 chsh -s $(which zsh)
@@ -15,7 +17,7 @@ chsh -s $(which zsh)
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# vim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c PlugInstall
+# neovim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+neovim -c PlugInstall
